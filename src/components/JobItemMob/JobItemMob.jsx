3171925dabe55ff2fa0e7cc83799106e92f2aaea
Oleksii Verbytskyi <alverba88@gmail.com>
2022-11-19 15:50:02 +0200

    mod mob list item

--- src/components/JobItemMob/JobItemMob.jsx
+++ src/components/JobItemMob/JobItemMob.jsx
@@ -1,6 +1,7 @@
 import styles from './JobItemMob.module.css';
 import {RatingStars} from "../UI/Rating";
 import {ReactComponent as BookMark} from "../../img/icons/Bookmark.svg";
+import {ReactComponent as LocationIcon} from "../../img/icons/Location.svg";
 
 export const JobItemMob = () => {
 
@@ -15,9 +16,24 @@ export const JobItemMob = () => {
         </div>
       </div>
 
-      <div className={styles.itemImg}>
-        <img className={styles.img} src="https://picsum.photos/200/300" alt="Sureplex"/>
+      <div className={styles.content}>
+        <div className={styles.itemImg}>
+          <img className={styles.img} src="https://picsum.photos/200/300" alt="Sureplex"/>
+        </div>
+        <div className={styles.description}>
+          <h2 className={styles.title}>Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat
+            nulla
+            ipsum eiusmod.</h2>
+          <p className={styles.name}>Department name • Sureplex</p>
+          <div className={styles.location}>
+            <LocationIcon className={styles.icon} width="13px" height="18px"/>
+            76 Blende Jardine Place
+          </div>
+        </div>
+
       </div>
+
+
     </li>
   )
 }
